a7d244bf8f4d0d744bfd9a7a774dc32f0a07d888
kit-lol <nikita.vitcenko42344@gmail.com>
2025-01-17 15:47:59 +0300

    потом напишу

---
+++ app/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
+		"equal: %d", u.Name, u.Age, u.Money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	tmpl, _ := template.ParseFiles("templates/main.html")
+	tmpl.Execute(w, bob)
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	// bob.setNewName("Alex")
+	// fmt.Fprintf(w, bob.getAllInfo())
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+}
+
+func maino() {
+	// var bob User = ..
+	// bob := User{name: "Bob", age: 50, money: -19, avg_grades: 4.2, happiness: 0.8}
+	handleRequest()
+
+}
